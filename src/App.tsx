c97a188c0a320162013f7d7156e223d2f3be6f83
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2020-10-26 16:13:37 +0300

    add react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import AuthContextProvider from './contexts/AuthContext';
 import Login from "./containers/login/Login";
+import { Route, Switch, BrowserRouter} from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
    <AuthContextProvider>
-      <Login/>
+     <BrowserRouter>
+       <Switch>
+         <Route path='/login' component={Login} />
+         {/*<Route path='/signup' component={SignUp} />*/}
+       </Switch>
+     </BrowserRouter>
    </AuthContextProvider>
   )
 }
